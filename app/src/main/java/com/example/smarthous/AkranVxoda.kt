92980ce0c04b68faefebc3c5dc98c998089c2548
Ходателев Игорь Александрович <pr120hodatelev@oat.local>
2023-11-28 16:25:42 +0600

    Commit2

---
+++ app/src/main/java/com/example/smarthous/AkranVxoda.kt
@@ -0,0 +1,11 @@
+package com.example.smarthous
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AkranVxoda : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_akran_vxoda)
+    }
+}
---
+++ app/src/main/java/com/example/smarthous/HomeAcran.kt
@@ -0,0 +1,11 @@
+package com.example.smarthous
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeAcran : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_acran)
+    }
+}
