2d7d949d8040c3fc192de6f92953b1e28ac9762a
Ходателев Игорь Александрович <pr120hodatelev@oat.local>
2023-11-29 16:33:01 +0600

    Commit3

---
+++ app/src/main/java/com/example/smarthous/AcranYstroistv.kt
@@ -0,0 +1,11 @@
+package com.example.smarthous
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AcranYstroistv : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_acran_ystroistv)
+    }
+}
---
+++ app/src/main/java/com/example/smarthous/Temperatura.kt
@@ -0,0 +1,11 @@
+package com.example.smarthous
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Temperatura : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_temperatura)
+    }
+}
